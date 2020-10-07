90e2f7c13e6b908b2e5b8dab5f44fab3ddf9e9dd
CoderJay06 <jaycru06@yahoo.com>
2020-10-06 20:40:04 -0400

    first commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Jay's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+
+            var stats = book.GetStatistics();
+            Console.WriteLine($"Highest grade: {stats.Low}");
+            Console.WriteLine($"Lowest grade: {stats.High}");
+            Console.WriteLine($"Average grade: {stats.Average:N1}");
+        }
+    }
+}
